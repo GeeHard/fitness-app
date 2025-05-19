deb49459c24be349f1e2cdad25b921218c8707c9
GeeHard <gerhardworch@gmail.com>
2025-05-19 10:42:20 +0200

    Use inline style for background image via PUBLIC_URL

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './LandingPage.css';
 
-const LandingPage = () => (
-  <div className='landing-page' />
-);
+const LandingPage = () => {
+  const bgUrl = process.env.PUBLIC_URL + '/background.jpg';
+  return (
+    <div
+      className='landing-page'
+      style={{ backgroundImage: `url(${bgUrl})` }}
+    />
+  );
+};
 
 export default LandingPage;
