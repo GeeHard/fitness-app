68641c18a72c81f4cb4aeb461d52d2fdd84a3459
GeeHard <gerhardworch@gmail.com>
2025-05-19 10:30:52 +0200

    Configure App.js for routing and remove default content

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import LandingPage from './components/LandingPage';
+import PushupsPage from './components/PushupsPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/pushups' element={<PushupsPage />} />
+      </Routes>
     </div>
   );
 }
